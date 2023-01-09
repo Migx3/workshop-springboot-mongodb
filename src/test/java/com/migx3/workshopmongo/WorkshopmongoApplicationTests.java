fc69d3bdfafe86705409b46aa4ea3405168bb9a3
Migx3 <lenad3vil@protonmail.com>
2023-01-09 15:11:10 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/migx3/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migx3.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
