c8e3eaffb2c71896cda944d4ad1348c29a94a4a0
Migx3 <lenad3vil@protonmail.com>
2023-01-10 14:23:09 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/migx3/workshopmongo/config/Instantion.java
+++ src/main/java/com/migx3/workshopmongo/config/Instantion.java
@@ -3,6 +3,7 @@ package com.migx3.workshopmongo.config;
 import com.migx3.workshopmongo.domain.Post;
 import com.migx3.workshopmongo.domain.User;
 import com.migx3.workshopmongo.dto.AuthorDTO;
+import com.migx3.workshopmongo.dto.CommentDTO;
 import com.migx3.workshopmongo.repositories.PostRepository;
 import com.migx3.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,9 +41,16 @@ public class Instantion implements CommandLineRunner {
         Post post1 = new Post(null, dateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, dateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO comment1 = new CommentDTO("Boa viagem mano!", dateFormat.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Aproveite!", dateFormat.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", dateFormat.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2, comment3));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
+
         userRepository.save(maria);
     }
 }
