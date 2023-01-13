56a87642fead3bb19d0c127bdaeb0397a53409c2
= <lenad3vil@protonmail.com>
2023-01-13 18:27:03 -0300

    Consulta com vários critérios

--- src/main/java/com/migx3/workshopmongo/resources/util/URL.java
+++ src/main/java/com/migx3/workshopmongo/resources/util/URL.java
@@ -2,10 +2,25 @@ package com.migx3.workshopmongo.resources.util;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
     public static String decodeParam(String text) {
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        try {
+            return dateFormat.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 }
